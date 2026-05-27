87d91bd8f8a501cce1e78082c010e304e2740ae3
Yohannes <jhonasfaw2@gmail.com>
2026-05-27 19:52:54 +0300

    fix api routes

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -1,21 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-
-    console.log("BOOKING RECEIVED:", body);
-
-    return NextResponse.json({
-      success: true,
-      message: "Booking received",
-    });
-  } catch (error) {
-    console.log("ERROR:", error);
-
-    return NextResponse.json({
-      success: false,
-      message: "Booking failed",
-    });
-  }
+export async function POST() {
+  return NextResponse.json({
+    success: true,
+  });
 }
